778d926f30a0f54fcd0b7e2894745006aa4b88cf
nandinimandli <mandlinandini.1124@gmail.com>
2025-07-04 20:01:54 +0530

    first commit

--- provider.tf
+++ provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.67.0"  # ✅ ONLY AWS 4.x works with module v19.15.1
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
 locals {
   region = "ap-south-1"
   name   = "cloud1-cluster"
@@ -6,22 +21,8 @@ locals {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+
   tags = {
     Environment = "dev"
   }
-}
-
-terraform {
-  required_version = ">= 1.3.0"  # 1.8.4 is fine
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.67.0"  # ✅ Downgraded for module v19.15.1 compatibility
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
 }

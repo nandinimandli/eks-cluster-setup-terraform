65872f83b8d136fce7a45055686d3d0abef98633
nandinimandli <mandlinandini.1124@gmail.com>
2025-07-04 20:10:09 +0530

    first commit

--- provider.tf
+++ provider.tf
@@ -1,28 +1,31 @@
-terraform {
-  required_version = ">= 1.3.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.67.0"  # ✅ ONLY AWS 4.x works with module v19.15.1
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
 locals {
   region = "ap-south-1"
-  name   = "cloud1-cluster"
+  name   = "netflix-cluster"
   vpc_cidr = "10.0.0.0/16"
   azs      = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
-
   tags = {
-    Environment = "dev"
+    Example = local.name
   }
+}
+
+
+
+terraform {
+  required_version = ">= 1.12.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.33.0"  # known stable version
+    }
+  }
+}
+
+
+
+provider "aws" {
+  region = "ap-south-1"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.19.0"
+  version = "~> 4.0"
 
   name = local.name
   cidr = local.vpc_cidr
@@ -19,5 +19,4 @@ module "vpc" {
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
   }
-  tags = local.tags
 }
